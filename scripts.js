01588f539f841dbaf1f23a26999e242d95ff2b81
babayagi <darsan_hirani@hotmail.com>
2019-09-04 00:33:32 +0300

    contact javascript trial 1

--- scripts.js
+++ scripts.js
@@ -130,24 +130,24 @@ function bb() {
 
 // contact validation
 // getting the name from form
-$(document).ready(function () {
-  $("#submit").click(function (event) {
+$(document).ready(function() {
+  $("#submit").click (function(event) {
     console.log("working")
     event.preventDefault();
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var text = document.getElementById("text").value;
+    var name=document.getElementById("name").value;
+    var email=document.getElementById("email").value;
+    var text=document.getElementById("text").value;
     // form validation
-    if (name == "") {
+    if (name == ""){
       alert("Please enter your name");
       return false;
-    } else if (email == "") {
+    }else if(email == ""){
       alert("Please enter your email")
       return false;
-    } else if (text == "") {
+    }else if(text == ""){
       alert("Please give a feedback")
       return false;
-    } else {
+    }else {
       alert(name + " your message is received. Thank you");
     }
     document.getElementById("form1").reset();
